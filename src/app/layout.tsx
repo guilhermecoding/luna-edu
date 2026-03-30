00d646cb3a6a0443976098b81eb0854fc0480db7
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-03-30 16:32:33 -0300

    :lipstick: feat: Remoção de código do boilerplate.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="pt-BR"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }

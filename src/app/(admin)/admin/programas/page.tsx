f3597799cfcbbdbe33d69e80cd7329a077cb816b
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-04-09 22:23:29 -0300

    :sparkles: feat: Início de implementação da página de períodos.

--- src/app/(admin)/admin/programas/page.tsx
+++ src/app/(admin)/admin/programas/page.tsx
@@ -1,6 +1,28 @@
+import Page from "@/components/page";
+import Section from "@/components/section";
+import TitlePage from "@/components/title-page";
+import { Button } from "@/components/ui/button";
+import { IconCirclePlusFilled } from "@tabler/icons-react";
 
 export default function ProgramsPage() {
     return (
-        <div>page</div>
+        <Page>
+            <Section>
+                <div className="flex flex-col lg:flex-row gap-y-6">
+                    <div className="flex-1">
+                        <TitlePage
+                            title="Programas"
+                            description="Gerencie os programas de aprendizado disponíveis na sua instituição."
+                        />
+                    </div>
+                    <div className="flex flex-1 justify-end items-end">
+                        <Button className="w-full sm:w-auto">
+                            <IconCirclePlusFilled className="size-5" />
+                            Criar Programa
+                        </Button>
+                    </div>
+                </div>
+            </Section>
+        </Page>
     );
 }
---
+++ src/components/title-page.tsx
@@ -0,0 +1,16 @@
+interface TitlePageProps {
+    title: string;
+    description?: string;
+}
+/**
+ * Título da página.
+ * @returns Componente de título e descrição (opcional) da página.
+ */
+export default function TitlePage({ title, description }: TitlePageProps) {
+    return (
+        <div>
+            <h1 className="text-3xl sm:text-4xl font-bold mb-2">{title}</h1>
+            {description && <p className="text-base sm:text-lg text-muted-foreground">{description}</p>}
+        </div>
+    );
+}

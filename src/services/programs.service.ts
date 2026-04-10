d5e844987f5e3f95e25ae5ba0907ff8b5a12cfd6
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-04-10 00:58:38 -0300

    :sparkles: feat: Adição de cache na busca dos programas.

--- src/services/programs.service.ts
+++ src/services/programs.service.ts
@@ -1,6 +1,11 @@
 import { Program } from "@/generated/prisma/client";
 import prisma from "@/lib/prisma";
+import { cacheLife, cacheTag } from "next/cache";
 
 export async function getPrograms(): Promise<Program[]> {
+    "use cache";
+    cacheLife("weeks");
+    cacheTag("programs");
+
     return await prisma.program.findMany();
 }

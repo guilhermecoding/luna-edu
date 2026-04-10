c0b5e46c949545c416757fe6c6bd6f116a6df730
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-04-09 23:54:25 -0300

    :wrench: chore: Correção de darkmode em dv.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,12 +32,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const darkModeDev = process.env.NEXT_PRUBLIC_DARK_MODE_DEV; // Testa o modo escuro em desenvolvimento
+  const darkModeDev = process.env.NEXT_PUBLIC_DARK_MODE_DEV; // Testa o modo escuro em desenvolvimento
 
   return (
     <html
       lang="pt-BR"
-      className={cn("h-full", "antialiased", poppins.variable, silkscreen.variable, darkModeDev && "dark")}
+      className={cn("h-full", "antialiased", poppins.variable, silkscreen.variable, darkModeDev === "true" ? "dark" : "")}
     >
       <body>
         <Providers>

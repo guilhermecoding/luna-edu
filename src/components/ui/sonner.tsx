015b4ac799d49e80c875bdb1c13b5a221b374578
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-04-10 16:48:30 -0300

    :sparkles: feat: Adição do componente Toaster utilizando a biblioteca Sonner e integração com temas do Next.js.

---
+++ src/components/ui/sonner.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { IconAlertCircleFilled, IconAlertOctagonFilled, IconCircleCheckFilled, IconInfoCircleFilled, IconLoader2 } from "@tabler/icons-react";
+import { useTheme } from "next-themes";
+import { Toaster as Sonner, type ToasterProps } from "sonner";
+
+
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme();
+
+  return (
+    <Sonner
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group"
+      icons={{
+        success: (
+          <IconCircleCheckFilled
+            className="size-4"
+          />
+        ),
+        info: (
+          <IconInfoCircleFilled
+            className="size-4"
+          />
+        ),
+        warning: (
+          <IconAlertCircleFilled
+            className="size-4"
+          />
+        ),
+        error: (
+          <IconAlertOctagonFilled
+            className="size-4"
+          />
+        ),
+        loading: (
+          <IconLoader2
+            className="size-4 animate-spin"
+          />
+        ),
+      }}
+      style={
+        {
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
+          "--border-radius": "var(--radius)",
+        } as React.CSSProperties
+      }
+      toastOptions={{
+        classNames: {
+          toast: "cn-toast",
+        },
+      }}
+      {...props}
+    />
+  );
+};
+
+export { Toaster };

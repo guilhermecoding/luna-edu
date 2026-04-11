be53f26d269f49f51ebaa05623b73dbe0272104b
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-04-10 22:26:49 -0300

    :sparkles: feat: Adição de configuração experimental para gerenciamento de tempos de cache dinâmico e estático

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   cacheComponents: true,
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+      static: 30,
+    },
+  },
   allowedDevOrigins: process.env.NEXT_PUBLIC_ALLOWED_DEV_ORIGINS
     ? process.env.NEXT_PUBLIC_ALLOWED_DEV_ORIGINS.split(",").map(o => o.trim())
     : [],

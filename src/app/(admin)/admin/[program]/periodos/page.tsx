bb6a056968eb4322333906cbf601e73b20c160e3
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-04-12 23:06:21 -0300

    feat: Substituir título e descrição por componente TitlePage na página de períodos

--- src/app/(admin)/admin/[program]/periodos/page.tsx
+++ src/app/(admin)/admin/[program]/periodos/page.tsx
@@ -1,5 +1,6 @@
 import Page from "@/components/page";
 import Section from "@/components/section";
+import TitlePage from "@/components/title-page";
 import { Button } from "@/components/ui/button";
 import { IconCalendarPlus } from "@tabler/icons-react";
 
@@ -7,15 +8,12 @@ export default function PeriodsPage() {
     return (
         <Page>
             <Section>
-                <h1 className="font-bold text-3xl md:text-4xl">
-                    Gestão de Períodos
-                </h1>
-                <div className="mt-2 w-full flex flex-col gap-4 lg:flex-row md:items-start">
-                    <h3 className="flex-1 md:max-w-3/4 text-muted-foreground text-sm md:text-base">
-                        Gerencie os períodos letivos do programa. Adicione, edite ou remova períodos conforme necessário para manter o calendário acadêmico atualizado.
-                    </h3>
-                    <div className="flex w-full lg:w-auto justify-end">
-                        <Button className="flex items-center w-full sm:w-auto gap-2 whitespace-nowrap">
+                <div className="flex flex-col lg:flex-row gap-y-6">
+                    <div className="flex-1">
+                        <TitlePage title="Períodos" description="Gerencie os períodos letivos do programa. Adicione, edite ou remova períodos conforme necessário para manter o calendário acadêmico atualizado." />
+                    </div>
+                    <div className="flex flex-1 justify-end items-end">
+                        <Button className="w-full sm:w-auto">
                             <IconCalendarPlus className="size-5" />
                             <span className="text-base mt-0.5">
                                 Adicionar Período

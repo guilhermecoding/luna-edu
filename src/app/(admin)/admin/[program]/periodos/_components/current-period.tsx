e559d3e68163088c0ed9c4eb4278b237a4f3c0da
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-04-14 19:23:57 -0300

    :sparkles: feat: Melhorar layout do componente CurrentPeriod com ajustes de estilo e estrutura

--- src/app/(admin)/admin/[program]/periodos/_components/current-period.tsx
+++ src/app/(admin)/admin/[program]/periodos/_components/current-period.tsx
@@ -13,7 +13,7 @@ function Info({
     info?: string
 }) {
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-start xl:items-center">
             <p className="text-sm font-medium uppercase text-muted-foreground flex flex-row gap-x-1 items-start">
                 {label}
                 {info && <TooltipText text={info}>
@@ -29,33 +29,35 @@ function Info({
 
 export default function CurrentPeriod() {
     return (
-        <div className="w-3/5 border border-muted-foreground/40 bg-slate-100 dark:bg-muted p-8 rounded-4xl space-y-3">
+        <div className="w-full xl:w-3/5 border border-muted-foreground/40 bg-slate-100 dark:bg-muted p-8 rounded-4xl">
             {/* Primeira linha */}
-            <div className="flex flew-row justify-between">
+            <div className="flex flex-col gap-y-3">
                 {/* Bagde */}
                 <div className="flex items-center">
-                    <span className="bg-primary-theme/20 text-sm font-bold text-primary-theme px-4 py-1 rounded-full">
-                        PERÍODO ATUAL
-                    </span>
+                    <div className="w-full flex flex-row justify-between">
+                        <span className="bg-primary-theme/20 text-sm font-bold text-primary-theme px-4 py-1 rounded-full">
+                            PERÍODO ATUAL
+                        </span>
+                        <PulsingStatusIndicator text="ATIVO" variant="success" />
+                    </div>
                 </div>
                 {/* Status */}
-                <div className="flex flex-col items-center">
-                    <PulsingStatusIndicator text="ATIVO" variant="success" />
+                <div className="flex justify-start sm:justify-end">
                     <span className="text-sm text-muted-foreground">
-                        Desde 14 de abril de 2024
+                        Desde 14 Abr, 2026
                     </span>
                 </div>
             </div>
 
             {/* Segunda linha */}
-            <div className="w-full">
-                <h1 className="font-bold capitalize text-5xl ">
+            <div className="w-full mt-8">
+                <h1 className="font-bold capitalize text-4xl xl:text-5xl ">
                     1 Ciclo de 2026
                 </h1>
             </div>
 
             {/* Terceira linha */}
-            <div className="flex justify-between mt-8">
+            <div className="flex flex-col xl:flex-row items-start gap-y-4 xl:justify-between mt-8">
                 <Info label="DISCIPLINAS" value={12} />
                 <Info label="EST. TOTAL" value={562} info="Total de estudantes adicionados neste período" />
                 <Info label="EST. MATRICULADOS" value={342} info="Estudantes vinculados em pelo menos uma disciplina deste período" />

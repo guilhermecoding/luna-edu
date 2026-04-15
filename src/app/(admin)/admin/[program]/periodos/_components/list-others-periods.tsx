a389b5677803d5ad5cce1d3159f3684024f63392
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-04-14 23:17:08 -0300

    :sparkles: feat: Melhorar o layout e a estrutura do componente ItemPeriod com ajustes de responsividade e informações

--- src/app/(admin)/admin/[program]/periodos/_components/list-others-periods.tsx
+++ src/app/(admin)/admin/[program]/periodos/_components/list-others-periods.tsx
@@ -32,53 +32,61 @@ function Info({
 
 function ItemPeriod() {
     return (
-        <div className="w-full bg-surface flex flex-row justify-between items-center border border-muted-foreground/40 p-4 rounded-4xl">
-            <div className="flex flex-row items-center">
-                {/* Banner */}
-                <div className="size-14 flex justify-center items-center rounded-xl bg-muted-foreground/30">
-                    <IconCalendarFilled className="text-muted-foreground/90 size-7" />
+        <div className="w-full min-w-0 flex flex-row items-center bg-surface border border-muted-foreground/40 p-4 rounded-4xl gap-3">
+            {/* Status (Celular) e Conteúdo */}
+            <div className="mb-4 flex min-w-0 flex-1 flex-col sm:mb-0">
+                {/* Status (Celular) */}
+                <StaticStatusIndicator className="flex sm:hidden ml-1 mb-2" text="FINALIZADO" variant="done" />
+
+                <div className="flex w-full @container/item min-w-0 flex-row items-center justify-between">
+                    <div className="flex w-full @sm/item:w-1/2 flex-row items-center">
+                        {/* Banner */}
+                        <div className="size-14 shrink-0 flex justify-center items-center rounded-xl bg-muted-foreground/30">
+                            <IconCalendarFilled className="text-muted-foreground/90 size-7" />
+                        </div>
+
+                        {/* Titulo e periodo */}
+                        <div className="ml-4 min-w-0 flex-1">
+                            <p className="truncate text-lg font-medium">
+                                1° Ciclo de 2026789012345677890029282028920282028202820282028202820282028202820
+                            </p>
+                            <p className="truncate text-sm font-medium text-muted-foreground/90">
+                                07 Ago, 2023 - 07 Set, 2023
+                            </p>
+                        </div>
+                    </div>
+
+                    {/* Infos */}
+                    <div className="hidden @3xl/main:flex flex-row items-center">
+                        <Info label="DICIPLINAS" value={15} />
+
+                        <Separator orientation="vertical" className="mx-4" />
+
+                        <Info label="ALUNOS" value={120} />
+                    </div>
+
+                    {/* Status (Tablet e Desktop) */}
+                    <StaticStatusIndicator className="hidden sm:flex" text="FINALIZADO" variant="done" />
+
+
                 </div>
-
-                {/* Titulo e periodo */}
-                <div className="ml-6">
-                    <p className="font-medium text-lg">1° Ciclo de 2026</p>
-                    <p className="text-sm font-medium text-muted-foreground/90">
-                        07 Ago, 2023 - 07 Set, 2023
-                    </p>
-                </div>
             </div>
-
-            {/* Infos */}
-            <div className="flex flex-row items-center">
-                <Info label="DICIPLINAS" value={15} />
-
-                <Separator orientation="vertical" className="mx-4" />
-
-                <Info label="ALUNOS" value={120} />
-            </div>
-
-            {/* Status e ações */}
-            <div className="flex flex-row gap-3 items-center">
-                {/* Status */}
-                <StaticStatusIndicator text="FINALIZADO" variant="done" />
-
-                {/* Ações */}
-                <Popover>
-                    <PopoverTrigger asChild>
-                        <Button variant="outline" className="bg-transparent border-none">
-                            <IconDotsVerticalFilled className="size-6 text-muted-foreground/90" />
-                        </Button>
-                    </PopoverTrigger>
-                    <PopoverContent align="end">
-                        <PopoverHeader>
-                            <PopoverTitle>Dimensions</PopoverTitle>
-                            <PopoverDescription>
-                                Set the dimensions for the layer.
-                            </PopoverDescription>
-                        </PopoverHeader>
-                    </PopoverContent>
-                </Popover>
-            </div>
+            {/* Ações */}
+            <Popover>
+                <PopoverTrigger asChild>
+                    <Button variant="outline" className="bg-transparent border-none p-0">
+                        <IconDotsVerticalFilled className="size-6 text-muted-foreground/90" />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent align="end">
+                    <PopoverHeader>
+                        <PopoverTitle>Dimensions</PopoverTitle>
+                        <PopoverDescription>
+                            Set the dimensions for the layer.
+                        </PopoverDescription>
+                    </PopoverHeader>
+                </PopoverContent>
+            </Popover>
         </div>
     );
 }

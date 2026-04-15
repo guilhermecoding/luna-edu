7f72ca7db0596496c518086feb352b87ea8574e8
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-04-15 19:43:48 -0300

    :sparkles: feat: Correção de recurepração de Data.

--- src/lib/format-date.ts
+++ src/lib/format-date.ts
@@ -1,10 +1,11 @@
 export default function formatDate(date: Date) {
-    const day = new Intl.DateTimeFormat("pt-BR", { day: "2-digit" }).format(date);
-    const month = new Intl.DateTimeFormat("pt-BR", { month: "short" })
+    const timeZone = "UTC";
+    const day = new Intl.DateTimeFormat("pt-BR", { day: "2-digit", timeZone }).format(date);
+    const month = new Intl.DateTimeFormat("pt-BR", { month: "short", timeZone })
         .format(date)
         .replace(".", "")
         .replace(/^./, (char) => char.toUpperCase());
-    const year = new Intl.DateTimeFormat("pt-BR", { year: "numeric" }).format(date);
+    const year = new Intl.DateTimeFormat("pt-BR", { year: "numeric", timeZone }).format(date);
 
     return `${day} ${month}, ${year}`;
-}
+}

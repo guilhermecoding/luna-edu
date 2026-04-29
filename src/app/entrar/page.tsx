51ef4093cfd784390d9abbcc5cf288402cbae0db
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-04-29 17:29:02 -0300

    :sparkles: feat: Adição de lógica para exibição condicional do logotipo da corporação na página de login. Implementação de estado para controlar a visibilidade do logotipo e tratamento de erro para ocultá-lo caso a imagem não carregue corretamente, melhorando a experiência do usuário.

--- src/app/entrar/page.tsx
+++ src/app/entrar/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 import LoginForm from "./_components/login-form";
 import GibbyAnimate from "@/components/gibby-animate";
 
 export default function LoginPage() {
     const logoCorporation = process.env.NEXT_PUBLIC_LOGO_CORPORATION;
+    const [showCorporationLogo, setShowCorporationLogo] = useState(Boolean(logoCorporation));
     return (
         <main className="min-h-screen w-full grid grid-cols-1 lg:grid-cols-2 bg-surface">
             {/* Lado Esquerdo - Imagem (Escondido em Mobile) */}
@@ -27,15 +29,18 @@ export default function LoginPage() {
                         <div className="flex flex-col items-center gap-2">
                             <div className="flex flex-row items-center gap-2">
                                 <GibbyAnimate className="size-24" />
-                                <span className="text-4xl font-bold text-muted-foreground mt-4 mr-2">+</span>
-                                {logoCorporation && (
-                                    <Image
-                                        src={logoCorporation}
-                                        alt="Logo"
-                                        width={100}
-                                        height={100}
-                                        className="size-20 object-contain mt-3"
-                                    />
+                                {showCorporationLogo && logoCorporation && (
+                                    <>
+                                        <span className="text-4xl font-bold text-muted-foreground mt-4 mr-2">+</span>
+                                        <Image
+                                            src={logoCorporation}
+                                            alt="Logo"
+                                            width={100}
+                                            height={100}
+                                            className="size-20 object-contain mt-3"
+                                            onError={() => setShowCorporationLogo(false)}
+                                        />
+                                    </>
                                 )}
                             </div>
                             <h2 className="font-silkscreen text-primary-theme text-6xl">LUNA</h2>

0dc669d2fed43dcbfac05459bceb17ed277084bb
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-05-05 10:45:08 -0300

    ✨ feat: Update remotePatterns configuration in next.config.ts to enhance image handling. Implement logic to parse and structure the NEXT_PUBLIC_LOGO_CORPORATION URL for improved remote image access.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
+import { RemotePattern } from "next/dist/shared/lib/image-config";
 
-const remotePatterns = [];
+const remotePatterns: RemotePattern[] = [];
 
 if (process.env.NEXT_PUBLIC_LOGO_CORPORATION) {
-    remotePatterns.push(
-        new URL(process.env.NEXT_PUBLIC_LOGO_CORPORATION),
-    );
+    const url = new URL(process.env.NEXT_PUBLIC_LOGO_CORPORATION);
+
+    remotePatterns.push({
+        protocol: url.protocol.replace(":", "") as "http" | "https",
+        hostname: url.hostname,
+        port: url.port,
+        pathname: "/**",
+    });
 }
 
 const nextConfig: NextConfig = {

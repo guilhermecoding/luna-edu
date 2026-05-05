e5a585c739dcb2aa756c751fd4bfddd12f652311
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-05-05 00:40:07 -0300

    :sparkles: feat: Atualização do arquivo .env.example para incluir a variável NEXT_PUBLIC_DARK_MODE_DEV e ajuste na configuração de imagens do Next.js para utilizar um array dinâmico de remotePatterns. Adição do pacote 'esbuild' à lista de dependências construídas no pnpm-workspace.yaml.

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,21 @@
 import type { NextConfig } from "next";
 
+const remotePatterns = [];
+
+if (process.env.NEXT_PUBLIC_LOGO_CORPORATION) {
+    remotePatterns.push(
+        new URL(process.env.NEXT_PUBLIC_LOGO_CORPORATION),
+    );
+}
+
 const nextConfig: NextConfig = {
-    /* config options here */
     reactCompiler: true,
     cacheComponents: true,
     allowedDevOrigins: process.env.NEXT_PUBLIC_ALLOWED_DEV_ORIGINS
         ? process.env.NEXT_PUBLIC_ALLOWED_DEV_ORIGINS.split(",").map(o => o.trim())
         : [],
     images: {
-        remotePatterns: [
-            new URL(process.env.NEXT_PUBLIC_LOGO_CORPORATION || ""),
-        ],
+        remotePatterns,
     },
 };
 
